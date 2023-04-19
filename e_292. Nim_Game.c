2c60180826b87e09354815512fa78f765d9997bb
Kishore <130527270+Zen-Bytes@users.noreply.github.com>
2023-04-19 18:10:18 +0530

    Create e_292. Nim_Game.c

---
+++ e_292. Nim_Game.c
@@ -0,0 +1,27 @@
+/*
+You are playing the following Nim Game with your friend:
+
+. Initially, there is a heap of stones on the table.
+. You and your friend will alternate taking turns, and you go first.
+. On each turn, the person whose turn it is will remove 1 to 3 stones from the heap.
+. The one who removes the last stone is the winner.
+
+Given n, the number of stones in the heap, return true if you can win the game assuming both you and your friend play optimally, otherwise return false.
+
+*/
+//when less than 3, u always win, while when more than four u can win, if its four u can't win
+
+bool canWinNim(int n){
+    if (n <= 3) {
+        return true;
+    }
+    if (n == 4) {
+        return false;
+    }
+    if(n % 4 != 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
